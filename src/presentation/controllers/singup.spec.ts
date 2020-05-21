901a9aa5c9d85e1557c9cf8ddf074f07bd398dd0
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-05-21 10:32:23 -0300

    refactor: move all erros imports to a single file

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -1,7 +1,5 @@
 import { SingUpController } from './singup'
-import { MissingParamError } from '../errors/missing-param-error'
-import { InvalidParamError } from '../errors/invalid-param-error'
-import { ServerError } from '../errors/server-error'
+import { MissingParamError, InvalidParamError, ServerError } from '../errors'
 import { EmailValidator } from '../protocols/email-validator'
 
 interface SutTypes {
