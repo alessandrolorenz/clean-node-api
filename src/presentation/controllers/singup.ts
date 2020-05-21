d5f40738d0575eb8539cb55c98991a474c8f3de4
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-05-21 10:27:15 -0300

    refactor: move sever error to a helper metod

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,10 +1,9 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { InvalidParamError } from '../errors/invalid-param-error'
-import { ServerError } from '../errors/server-error'
 
 export class SingUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -25,10 +24,8 @@ export class SingUpController implements Controller {
         return badRequest(new InvalidParamError('email'))
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      // console.log(error)
+      return serverError()
     }
   }
 }
