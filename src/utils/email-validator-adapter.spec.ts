c8d6af9b8bef06d72c823dee1238620123549c2d
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-05-24 16:24:49 -0300

    test: ensure EmailValidatorAdapter returns false

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,9 @@
+import { EmailValidatorAdapter } from './email-validator'
+
+describe('EmailValidator Adapter', () => {
+  test('Should return false if validator returns false', () => {
+    const stu = new EmailValidatorAdapter()
+    const isValid = stu.isValid('invalid@gmail.com')
+    expect(isValid).toBe(false)
+  })
+})
