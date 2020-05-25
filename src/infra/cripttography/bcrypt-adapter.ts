c3a4f800ad4cba82764145fdd03ca8e4a8a6efab
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-05-25 17:09:37 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/cripttography/bcrypt-adapter.ts
+++ src/infra/cripttography/bcrypt-adapter.ts
@@ -10,7 +10,6 @@ export class BcryptAdapter implements Encrypter {
 
   async encrypt (value: string): Promise<string> {
     const hash = await bcrypt.hash(value, this.salt)
-
     return hash
   }
 }
