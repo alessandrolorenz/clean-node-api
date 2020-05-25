d685ab1655a6f816f5f799306721af83176fe92e
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-05-25 11:01:01 -0300

    feat: ensure DbAddAccount call AddAccount Repository w/ correct values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,15 +1,20 @@
 import { AddAccount, AddAccountModel, AccountModel, Encrypter } from './add-account-protocols'
+import { AddAccountRepository } from '../../protocols/add-account-repository'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
+  private readonly addAccountRepository: AddAccountRepository
 
-  constructor (encrypter: Encrypter) {
+  constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
     this.encrypter = encrypter
+    this.addAccountRepository = addAccountRepository
   }
 
   // erros são tratados no controller com o try catch (deve ser feito um teste garantindo que não sera tratado aqui)
-  async add (account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password)
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassowrd = await this.encrypter.encrypt(accountData.password)
+    await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassowrd }))
+    // Obj.assign = cria um novo obj ({} garante que não vai alterar o obj passado, o obj, e o valor a substituir)
     return new Promise(resolve => resolve(null))
   }
 }
