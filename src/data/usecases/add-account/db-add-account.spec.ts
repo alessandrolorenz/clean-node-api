6c00cc9935e1d4abb88b7e96816914553b63679a
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-05-25 09:25:07 -0300

    refactor: move sut creation to a helper method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,14 +1,28 @@
 import { DbAddAccount } from './db-add-account'
+import { Encrypter } from '../../protocols/encrypter'
+
+interface SutTypes {
+  sut: DbAddAccount
+  encrypertStub: Encrypter
+}
+
+const makeSut = (): SutTypes => {
+  class EncrypterStub {
+    async encrypt (value: string): Promise<string> {
+      return new Promise(resolve => resolve('hashedPassorwd'))
+    }
+  }
+  const encrypertStub = new EncrypterStub()
+  const sut = new DbAddAccount(encrypertStub)
+  return {
+    sut,
+    encrypertStub
+  }
+}
 
 describe('DbAddAccount UseCase', () => {
   test('Should call Encrypter with correct passowr', async () => {
-    class EncrypterStub {
-      async encrypt (value: string): Promise<string> {
-        return new Promise(resolve => resolve('hashedPassorwd'))
-      }
-    }
-    const encrypertStub = new EncrypterStub()
-    const sut = new DbAddAccount(encrypertStub)
+    const { sut, encrypertStub } = makeSut()
     const encryptSpy = jest.spyOn(encrypertStub, 'encrypt')
     const accountData = {
       name: 'valid_name',
