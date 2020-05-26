cc5e2a8ba237ba5ca5c60dcdd7212b9d8abc353a
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-05-26 15:07:11 -0300

    refactor: move group middleware interfacer to a single file

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,5 @@
 import { Express } from 'express'
-import { bodyParser } from '../middlewares/body-parser'
-import { cors } from '../../main/middlewares/cors'
-import { contentType } from '../../main/middlewares/content-type'
+import { bodyParser, cors, contentType } from '../middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
