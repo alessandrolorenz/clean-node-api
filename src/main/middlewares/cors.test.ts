bf41483eba71cce03bacccaefe825a614d2dc909
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-05-26 15:01:31 -0300

    feat: ensure requests return json as default type

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -3,11 +3,11 @@ import app from '../config/app'
 
 describe('CORS Middleware', () => {
   test('should enable CORS', async () => {
-    app.get('/test_body_parser', (req, res) => {
+    app.get('/test_cors', (req, res) => {
       res.send()
     })
     await request(app)
-      .get('/test_body_parser')
+      .get('/test_cors')
       .expect('access-control-allow-origin', '*')
       .expect('access-control-allow-methods', '*')
       .expect('access-control-allow-headers', '*')
