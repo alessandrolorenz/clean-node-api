c680ad4d41c222501c3abb4b73d3469ff9a16b86
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-05-25 21:10:00 -0300

    refactor: move mongodb methods to a helper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,19 @@
+import { MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  // como é typescript, a sintaxe do objeto (criado aqui) conflita com a forma de setar o tipo de uma variavel em TS
+  // então seta null e dai atribui
+  client: null as MongoClient,
+
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+  }
+
+}
