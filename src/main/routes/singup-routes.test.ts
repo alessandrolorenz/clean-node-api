682cf1084bf9d025eb8b62508952c5daef87e28c
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-05-26 16:01:11 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/singup-routes.test.ts
+++ src/main/routes/singup-routes.test.ts
@@ -1,7 +1,21 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('Singup routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accontCollection = MongoHelper.getCollection('accounts')
+    await accontCollection.deleteMany({}) // limpa a tabela
+  })
+
   test('should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
