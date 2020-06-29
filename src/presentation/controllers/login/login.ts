ace10968adac5225a776ab6840d80dea60266942
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-06-29 19:43:38 -0300

    feat: ensure LoginController calls EmailVAlidator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,12 +1,20 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { badRequest } from '../../helpers/http-helper'
 import { MissingParamError } from '../../errors'
+import { EmailValidator } from '../singup/singup-protocols'
 
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { email, password } = httpRequest.body
     if (!email || !password) {
       return new Promise(resolve => resolve(badRequest(new MissingParamError(email ? 'password' : 'email'))))
     }
+    this.emailValidator.isValid(email)
   }
 }
