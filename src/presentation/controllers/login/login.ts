75b68f381c6b165aa6799da14bda663eacb7d72d
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-06-29 17:38:56 -0300

    feat: ensure LoginController returns 400 if no password is privided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -4,6 +4,9 @@ import { MissingParamError } from '../../errors'
 
 export class LoginController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
+    const { email, password } = httpRequest.body
+    if (!email || !password) {
+      return new Promise(resolve => resolve(badRequest(new MissingParamError(email ? 'password' : 'email'))))
+    }
   }
 }
