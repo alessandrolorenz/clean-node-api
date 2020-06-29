63745ef0fc4b2897ff978eab05d44208bd39e23e
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-06-29 17:27:02 -0300

    test: ensure LoginController returns 400 if no email is privided

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,18 @@
+import { LoginController } from '../../../presentation/controllers/login/login'
+import { badRequest } from '../../helpers/http-helper'
+import { MissingParamError } from '../../errors'
+
+const makeSut = (): LoginController => new LoginController()
+
+describe('Login Controller', () => {
+  test('should return 400 if no email is provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+})
