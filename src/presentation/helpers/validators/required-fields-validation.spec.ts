ac080730e742da64739056f355bd75877c991ea6
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-07-14 16:45:29 -0300

    test: ensure RequiredFieldValidation not return is validation succeeds

--- src/presentation/helpers/validators/required-fields-validation.spec.ts
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -7,4 +7,9 @@ describe('RequiredField Validator', () => {
     const error = sut.validate({ email: 'email@email.com' })
     expect(error).toEqual(new MissingParamError('field'))
   })
+  test('Should not return if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
