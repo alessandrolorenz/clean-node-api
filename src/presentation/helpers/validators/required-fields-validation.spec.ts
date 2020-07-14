0d893479129ebf1ff0fc3a8640d44476d9c70bfc
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-07-14 16:42:51 -0300

    test: ensure RequiredFieldValidation returns a MissingParamError

---
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -0,0 +1,10 @@
+import { RequiredFieldValidation } from './required-field-validation'
+import { MissingParamError } from '../../errors'
+
+describe('RequiredField Validator', () => {
+  test('Should returtn a MIssingParamError if validator fails', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ email: 'email@email.com' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+})
