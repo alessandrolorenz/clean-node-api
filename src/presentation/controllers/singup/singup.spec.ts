daa9720e839b11ef627d4e252a46b2ff831b4271
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-07-13 21:04:50 -0300

    test: ensure SignupController returns 400 if validation fails

--- src/presentation/controllers/singup/singup.spec.ts
+++ src/presentation/controllers/singup/singup.spec.ts
@@ -194,4 +194,11 @@ describe('SingUp Controller', () => {
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
+
+  test('should return 400 if validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeHttpRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
 })
