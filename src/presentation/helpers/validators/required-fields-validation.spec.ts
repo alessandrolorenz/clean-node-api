fd57b3afd43ed9a236e2e62ba25b177cfceebe85
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2020-07-14 16:49:35 -0300

    refactor: move sut creation to a factory

--- src/presentation/helpers/validators/required-fields-validation.spec.ts
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -1,14 +1,16 @@
 import { RequiredFieldValidation } from './required-field-validation'
 import { MissingParamError } from '../../errors'
 
+const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation('field')
+
 describe('RequiredField Validator', () => {
   test('Should returtn a MIssingParamError if validator fails', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({ email: 'email@email.com' })
     expect(error).toEqual(new MissingParamError('field'))
   })
   test('Should not return if validation succeeds', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({ field: 'any_value' })
     expect(error).toBeFalsy()
   })
